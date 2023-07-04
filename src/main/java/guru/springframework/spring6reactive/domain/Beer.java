87742160afce819eaa17f1b2edd89870ba520ca8
thakkardhavalc <thakkardhavalc@gmail.com>
2023-07-04 12:50:15 -0600

    created Beer POJO and annotated id

---
+++ src/main/java/guru/springframework/spring6reactive/domain/Beer.java
@@ -0,0 +1,30 @@
+package guru.springframework.spring6reactive.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * Created By dhaval on 2023-07-04
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Beer {
+
+    @Id
+    private Integer id;
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
