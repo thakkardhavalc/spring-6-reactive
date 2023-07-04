9a2cf90e4e00a9248a43460ffeee6502f7281f1b
thakkardhavalc <thakkardhavalc@gmail.com>
2023-07-04 12:58:27 -0600

    created BeerRepositoryTest added simple test

---
+++ src/test/java/guru/springframework/spring6reactive/repositories/BeerRepositoryTest.java
@@ -0,0 +1,40 @@
+package guru.springframework.spring6reactive.repositories;
+
+import guru.springframework.spring6reactive.domain.Beer;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created By dhaval on 2023-07-04
+ */
+@Slf4j
+@DataR2dbcTest
+class BeerRepositoryTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testSaveNewBeer() {
+        beerRepository.save(getTestBeer())
+                .subscribe(beer -> {
+                    log.info(beer.toString());
+                });
+    }
+
+    Beer getTestBeer() {
+        return Beer.builder()
+                .beerName("Space Dust")
+                .beerStyle("IPA")
+                .upc("123123")
+                .price(BigDecimal.TEN)
+                .quantityOnHand(12)
+                .build();
+    }
+}
