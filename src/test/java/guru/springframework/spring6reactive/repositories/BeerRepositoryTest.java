184180eb20744ad20aae5be42f4c7ef724ef13de
thakkardhavalc <thakkardhavalc@gmail.com>
2023-07-05 12:42:31 -0600

    created json test

--- src/test/java/guru/springframework/spring6reactive/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6reactive/repositories/BeerRepositoryTest.java
@@ -1,5 +1,7 @@
 package guru.springframework.spring6reactive.repositories;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.spring6reactive.config.DatabaseConfig;
 import guru.springframework.spring6reactive.domain.Beer;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +22,13 @@ class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testCreateJson() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        log.info(objectMapper.writeValueAsString(getTestBeer()));
+    }
 
     @Test
     void testSaveNewBeer() {
